feba2eddd928a990939ecdc7ef99137f48a38201
Adrian Alvarez <193550904+adal001@users.noreply.github.com>
2025-12-18 18:16:42 -0700

    Implement round logic for the game
    Add playRound function to evaluate human and computer choices and determine
    win, loss, or tie outcomes. This establishes the core game flow and enables a
    complete round to be played using existing input functions.

--- javascript.js
+++ javascript.js
@@ -27,3 +27,39 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+// Logic to play a round of the game:
+function playRound(humanChoice, computerChoice) {
+    // Make human choice lower case to make different variations of input work:
+    humanChoice = humanChoice.toLowerCase();
+    // Computer chooses rock:
+    if (computerChoice === "rock" && humanChoice === "paper") {
+        console.log("You win! Paper beats Rock!");
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log("You lose! Rock beats Scissors!");
+    } else if (computerChoice === "rock" && humanChoice === "rock") {
+        console.log("It's a tie! Rock loves Rock!");
+    } // Computer chooses paper:
+      else if (computerChoice === "paper" && humanChoice === "scissors") {
+        console.log("You win! Scissors beats Paper!");
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log("You lose! Paper beats Rock!");
+    } else if (computerChoice === "paper" && humanChoice === "paper") {
+        console.log("It's a tie! Paper loves Paper!");
+    } // Computer chooses scissors:
+      else if (computerChoice === "scissors" && humanChoice === "rock") {
+        console.log("You win! Rock beats Scissors!");
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log("You lose! Scissors beats Paper!");
+    } else {
+        console.log("It's a tie! Scissors loves Scissors!");
+    }
+}
+
+// Use the human and computer choices to play a round of the game:
+const humanSelection = getHumanChoice();
+console.log(`Your choice: ${humanSelection}`);
+const computerSelection = getComputerChoice();
+console.log(`Computer's choice: ${computerSelection}`);
+
+playRound(humanSelection, computerSelection);
